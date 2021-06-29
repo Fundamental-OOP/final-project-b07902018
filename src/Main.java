b38c67b4dec484ef29e1da8b5b2a872958a93e95
Carlos Chen <carlos.chen.yaya@gmail.com>
2021-06-29 13:54:19 +0800

    raw menu

--- src/Main.java
+++ src/Main.java
@@ -34,31 +34,31 @@ public class Main {
         addAudioByFilePath(Walking.AUDIO_STEP1, new File("assets/audio/step1.wav"));
         addAudioByFilePath(Walking.AUDIO_STEP2, new File("assets/audio/step2.wav"));
 
-        List<Sprite> Sprites = new ArrayList<>();
+        // List<Sprite> Sprites = new ArrayList<>();
         // initialization procedure
-        Character p1 = new Character(new Point(0, 0));
-        Character p2 = new Character(new Point(300, 300));
-        Sprites.add(p1);
-        Sprites.add(p2);
-        Sprites.add(new Table(new Point(300, 0)));
-        Sprites.add(new ApplePieStove(new Point(300, 150)));
-        Sprites.add(new AppleFactory(new Point(0, 150)));
-        Sprites.add(new PieFactory(new Point(0, 300)));
-        Sprites.add(new VegetableFactory(new Point(150, 0)));
-        Sprites.add(new TrashCan(new Point(150, 300)));
+        // Character p1 = new Character(new Point(0, 0));
+        // Character p2 = new Character(new Point(300, 300));
+        // Sprites.add(p1);
+        // Sprites.add(p2);
+        // Sprites.add(new Table(new Point(300, 0)));
+        // Sprites.add(new ApplePieStove(new Point(300, 150)));
+        // Sprites.add(new AppleFactory(new Point(0, 150)));
+        // Sprites.add(new PieFactory(new Point(0, 300)));
+        // Sprites.add(new VegetableFactory(new Point(150, 0)));
+        // Sprites.add(new TrashCan(new Point(150, 300)));
 
-        OrderList o1 = new OrderList();
-        o1.addOrder(new ApplePie(new Point(0, 0)));
-        ScoreComputer scoreComputer = new ScoreComputer(new ArrayList<>());
-        scoreComputer.addScoreConversion(new ScoreApplePie(new ApplePie(null)));
+        // OrderList o1 = new OrderList();
+        // o1.addOrder(new ApplePie(new Point(0, 0)));
+        // ScoreComputer scoreComputer = new ScoreComputer(new ArrayList<>());
+        // scoreComputer.addScoreConversion(new ScoreApplePie(new ApplePie(null)));
 
-        ScoreBoard scoreboard = new ScoreBoard(0, 10, 10);
-        // PickupWindow window = new PickupWindow(new Point(450, 450), o1, scoreboard, scoreComputer);
-        Sprites.add(new PickupWindow(new Point(450, 450), o1, scoreboard, scoreComputer));
-        World world = new World(new CharacterCollisionHandler(), scoreboard, Sprites);  // model
-        Game game = new Game(world, p1, p2);  // controller
-        GameView view = new GameView(game);  // view
-        game.start();  // run the game and the game loop
-        view.launch(); // launch the GUI
+        // Scoreboard scoreboard = new Scoreboard(0, 100, 100);
+        // Sprites.add(new PickupWindow(new Point(450, 450), o1, scoreboard, scoreComputer));
+        // World world = new World(new CharacterCollisionHandler(), scoreboard, Sprites);  // model
+        // Game game = new Game(world, p1, p2);  // controller
+        Game game = new Game();
+        // GameView gameview = new GameView(game);  // view
+        game.start(game);  // run the game and the game loop
+        // view.launch(); // launch the GUI
     }
 }
--- src/controller/GameLoop.java
+++ src/controller/GameLoop.java
@@ -1,8 +1,22 @@
 package controller;
 
 import model.World;
+import views.GameView;
 import views.View;
+import controller.Game;
+import java.util.List;
 
+import javax.swing.JPanel;
+
+import java.util.ArrayList;
+import model.Sprite;
+import character.Character;
+import java.awt.*;
+import item.mobileItem.*;
+import item.staticItem.*;
+import order.OrderList;
+import scoring.*;
+import character.*;
 /**
  * @author - johnny850807@gmail.com (Waterball)
  */
@@ -16,8 +30,34 @@ public abstract class GameLoop {
         this.view = view;
     }
 
-    public void start() {
+    public void start(Game game) {
+        GameView gameview = new GameView(game);
+        gameview.menu();
+
+        List<Sprite> Sprites = new ArrayList<>();
+        Character p1 = new Character(new Point(0, 0));
+        Character p2 = new Character(new Point(300, 300));
+        Sprites.add(p1);
+        Sprites.add(p2);
+        Sprites.add(new Table(new Point(300, 0)));
+        Sprites.add(new ApplePieStove(new Point(300, 150)));
+        Sprites.add(new AppleFactory(new Point(0, 150)));
+        Sprites.add(new PieFactory(new Point(0, 300)));
+        Sprites.add(new VegetableFactory(new Point(150, 0)));
+        Sprites.add(new TrashCan(new Point(150, 300)));
+        OrderList o1 = new OrderList();
+        o1.addOrder(new ApplePie(new Point(0, 0)));
+        ScoreComputer scoreComputer = new ScoreComputer(new ArrayList<>());
+        scoreComputer.addScoreConversion(new ScoreApplePie(new ApplePie(null)));
+
+        ScoreBoard scoreboard = new ScoreBoard(0, 100, 100);
+        Sprites.add(new PickupWindow(new Point(450, 450), o1, scoreboard, scoreComputer));
+
+        World world = new World(new CharacterCollisionHandler(), scoreboard, Sprites);
+        game.construct(world, p1, p2);
+        
         new Thread(this::gameLoop).start();
+        gameview.launch();
     }
 
     private void gameLoop() {
@@ -43,6 +83,5 @@ public abstract class GameLoop {
             e.printStackTrace();
         }
     }
-
 
 }
