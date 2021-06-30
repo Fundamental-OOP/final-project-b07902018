9cfdb359936fd41323428c5f3bea705e32aefdf2
Timothy Chen <b07902025@csie.ntu.edu.tw>
2021-06-30 14:17:39 +0800

    remove temp file, add timer class.

---
+++ src/timer/Timer.java
@@ -0,0 +1,28 @@
+package timer;
+
+import javax.print.DocFlavor.STRING;
+
+public class Timer {
+    long endTime;
+    boolean started = false;
+
+
+    public String getCountString(){
+        if(started){
+            if(ended())return "00:00";
+            else {
+                long s = (endTime - System.currentTimeMillis())/1000;
+                return String.format("%02d:%02d", s/60,s%60);
+            }
+        }else{
+            return "99:99";
+        }
+    }
+    public boolean ended(){
+        return started&&System.currentTimeMillis()>=endTime;
+    }
+    public void startTimer(int sec){
+        started = true;
+        endTime = 1000*sec + System.currentTimeMillis();
+    }
+}
