4a081e268b26c069a17a303b867471a468f687f4
Doadaodao <81339151+Doadaodao@users.noreply.github.com>
2021-06-30 17:34:31 +0800

    consumer

--- src/order/Consumer.java
+++ src/order/Consumer.java
@@ -19,22 +19,22 @@ public abstract class Consumer {
             MobileItem item;
             switch (random) {
                 case 0:
-                    item = new FriedEgg(null);
+                    item = new FriedEgg(null, null);
                     break;
                 case 1:
-                    item = new CheeseEggSandwich(null);
+                    item = new CheeseEggSandwich(null, null);
                     break;
                 case 2:
-                    item = new Salad(null);
+                    item = new Salad(null, null);
                     break;
                 case 3:
-                    item = new RawEgg(null);
+                    item = new RawEgg(null, null);
                     break;
                 case 4:
-                    item = new ApplePie(null);
+                    item = new ApplePie(null, null);
                     break;
                 default:
-                    item = new Apple(null);
+                    item = new Apple(null, null);
             }
             String itemName = ((Ingredient) item).getIngredientName();
             System.out.printf("Order: %s%n", itemName);
