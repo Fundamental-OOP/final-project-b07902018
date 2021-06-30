d6f75165cf50f64e5ef46bbd1e9875e05e88472f
Carlos Chen <carlos.chen.yaya@gmail.com>
2021-06-30 23:27:31 +0800

    add CharacterShape in Character constructor

--- src/controller/GameLoop.java
+++ src/controller/GameLoop.java
@@ -79,14 +79,14 @@ public abstract class GameLoop {
         }
 
         if(gameview.getMenu().getPlayernum() == 1){
-            Character p1 = new Character(world.defaultPlayer1Location());
+            Character p1 = new Character(world.defaultPlayer1Location(), world.getCharacterShape());
             players.add(p1);
             //Sprites.add(p1);
             world.addSprite(p1);
         }
         else{
-            Character p1 = new Character(world.defaultPlayer1Location());
-            Character p2 = new Character(world.defaultPlayer2Location());
+            Character p1 = new Character(world.defaultPlayer1Location(), world.getCharacterShape());
+            Character p2 = new Character(world.defaultPlayer2Location(), world.getCharacterShape());
             players.add(p1);
             players.add(p2);
             //Sprites.add(p1);
