a14fb7a5c97f7b7e8dbc0a3811abd86075d1de7c
timothychen <45344933+timothychen1999@users.noreply.github.com>
2021-07-01 22:01:47 +0800

    Scoreboard position fix

--- src/scoring/ScoreBoard.java
+++ src/scoring/ScoreBoard.java
@@ -54,7 +54,7 @@ public class ScoreBoard extends Sprite {
     public void render(Graphics g) {
         g.setColor(Color.BLACK);
         g.setFont(new Font("Marker Felt", Font.PLAIN, 30));
-        g.drawString(String.format("%03d", getScore()), x, y);
+        g.drawString(String.format("%03d", getScore()), x+10, y);
     }
 
     @Override
