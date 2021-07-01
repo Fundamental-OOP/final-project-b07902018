9abd352de5f5ce307b895cab98499ae62e713212
b07508005 <68584277+b07508005@users.noreply.github.com>
2021-07-01 16:05:16 +0800

    new annoying world4

--- src/model/WorldExample4.java
+++ src/model/WorldExample4.java
@@ -4,7 +4,11 @@ import java.awt.Dimension;
 import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
+
+
 import javax.swing.JPanel;
+import timer.Timer;
+
 import item.mobileItem.ingredient.*;
 import item.staticItem.*;
 import item.staticItem.abandoningItem.PickupWindow;
@@ -13,10 +17,10 @@ import item.staticItem.craftingItem.ApplePieStove;
 import item.staticItem.craftingItem.FriedEggStove;
 import item.staticItem.craftingItem.SaladBowl;
 import item.staticItem.craftingItem.SandwichMaker;
-import item.staticItem.factoryItem.AppleBox;
 import item.staticItem.factoryItem.BreadBasket;
 import item.staticItem.factoryItem.CheeseBlock;
 import item.staticItem.factoryItem.EggBasket;
+import item.staticItem.factoryItem.FruitBasket;
 import item.staticItem.factoryItem.PieBox;
 import item.staticItem.factoryItem.SpinachGarden;
 import item.staticItem.factoryItem.TomatoBasket;
@@ -30,15 +34,41 @@ public class WorldExample4 extends World {
 
     private final int gridHeight = 75;
 
-    private final SpriteShape staticItemShape = new SpriteShape(new Dimension(75, 75), new Dimension(10, 10),
-            new Dimension(55, 55));
+    private final SpriteShape staticItemShape 
+        = new SpriteShape(new Dimension(75, 75), new Dimension(10, 10), new Dimension(55, 55));
 
-    private final SpriteShape mobileItemShape = new SpriteShape(new Dimension(30, 30), new Dimension(0, 0),
-            new Dimension(30, 30));
-
-    public WorldExample4(CollisionHandler collisionHandler, int width, int height,
-            List<Sprite> sprites, JPanel panel) {
+    private final SpriteShape mobileItemShape
+        = new SpriteShape(new Dimension(30, 30), new Dimension(0, 0), new Dimension(30, 30));
+    
+    public WorldExample4(CollisionHandler collisionHandler, int width, int height, List<Sprite> sprites, JPanel panel) {
         super(collisionHandler, width, height, sprites, panel);
+
+        // setting for outside game panel stuffs
+        // including timer, scoreboard, recipe, orderlist
+        // if you follow the size setting of world example 3
+        // then only modifiy the path of the .png files
+        var recipePicture = new FixedImageDisplayer("assets/worldexample4/recipe.png", 20 + 900, 720 - 180 * 2043 / 915 + 20, 180, 180 * 2043 / 915, panel);
+        var timerBackground = new FixedImageDisplayer("assets/worldexample4/timer.png", 20 + 900, 0, 180, 138, panel);
+        var scoreboardBackground = new FixedImageDisplayer("assets/worldexample4/scoreboard.png",20 + 900, 140, 180, 180,panel);
+        var orderListBackground = new FixedImageDisplayer("assets/worldexample4/orderlistbg.png", 0, 600, 900, 120, panel);
+
+        addSprite(recipePicture);
+        addSprite(timerBackground);
+        addSprite(scoreboardBackground);
+        addSprite(orderListBackground);
+
+        this.scoreboard = new ScoreBoard(0, gridWidth * 13, gridHeight * 14 / 5);
+        setScoreboard(scoreboard);
+        
+        this.timer = new Timer();
+        this.timerDisplayer = new TextDisplayer(computeXCoordinate(12.7), computeYCoordinate(1.2));
+        this.timerDisplayer.setText("Timer");
+        this.timerDisplayer.setFontSize(25);
+        addSprite(timerDisplayer);
+     
+        //
+        // the part you actually design your map
+        //
 
         for (int i = 0; i <= 8; ++i)
             addSprite(new Barrel(computeCoordinate(i, 0), staticItemShape));
@@ -50,7 +80,7 @@ public class WorldExample4 extends World {
         addSprite(new CheeseBlock(computeCoordinate(3, 2), staticItemShape, mobileItemShape));
         addSprite(new SpinachGarden(computeCoordinate(4, 2), staticItemShape, mobileItemShape));
         addSprite(new PieBox(computeCoordinate(5, 2), staticItemShape, mobileItemShape));
-        addSprite(new AppleBox(computeCoordinate(6, 2), staticItemShape, mobileItemShape));
+        addSprite(new FruitBasket(computeCoordinate(6, 2), staticItemShape, mobileItemShape));
         addSprite(new TomatoBasket(computeCoordinate(7, 2), staticItemShape, mobileItemShape));
 
         for (int i = 1; i <= 8; ++i)
@@ -68,10 +98,8 @@ public class WorldExample4 extends World {
         addSprite(new Plant1(computeCoordinate(9, 1), staticItemShape));
         addSprite(new Plant1(computeCoordinate(9, 2), staticItemShape));
         addSprite(new Plant1(computeCoordinate(9, 3), staticItemShape));
-        addSprite(new Plant1(computeCoordinate(9, 8), staticItemShape));
-        addSprite(new Plant1(computeCoordinate(9, 9), staticItemShape));
-        addSprite(new Plant1(computeCoordinate(9, 10), staticItemShape));
-
+        addSprite(new Plant1(computeCoordinate(10, 4), staticItemShape));
+        
         ScoreComputer scoreComputer = new ScoreComputer(new ArrayList<>());
         scoreComputer.addScoreConversion(new ApplePie(null, null), 30);
         scoreComputer.addScoreConversion(new FriedEgg(null, null), 10);
@@ -80,33 +108,38 @@ public class WorldExample4 extends World {
         scoreComputer.addScoreConversion(new CheeseEggSandwich(null, null), 50);
 
         PickupWindow window = new PickupWindow(computeCoordinate(9, 4), staticItemShape, scoreboard, scoreComputer);
-        addSprite(new Plant1(computeCoordinate(10, 4), staticItemShape));
-
-        addSprite(window);
+        addSprite(window); 
         //scoreboard.setX(1050);
         addSprite(new OrderDiplayer(50, 600, window, panel));
 
-
+        
     }
 
-    public Point computeCoordinate(int Xgrid, int Ygrid) {
-        return new Point(Xgrid * gridWidth, Ygrid * gridHeight);
+    public int computeXCoordinate(double Xgrid){
+        return (int) (Xgrid * gridWidth);
+    }
+
+    public int computeYCoordinate(double Ygrid){
+        return (int) (Ygrid * gridWidth);
+    }
+
+    public Point computeCoordinate(double Xgrid, double Ygrid){
+        return new Point((int) (Xgrid * gridWidth), (int) (Ygrid * gridHeight));
     }
 
     @Override
     public Point defaultPlayer1Location() {
-        return computeCoordinate(7, 6);
+        return computeCoordinate(8, 6);
     }
 
     @Override
     public Point defaultPlayer2Location() {
-        return computeCoordinate(9, 5);
+        return computeCoordinate(9, 6);
     }
-
+    
     @Override
     public SpriteShape getCharacterShape(){
         return new SpriteShape(new Dimension(146 / 2, 176 / 2),
                 new Dimension(40 / 2, 38 / 2), new Dimension(66 / 2, 104 / 2));
     }
-
 }
